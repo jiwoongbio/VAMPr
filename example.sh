68175a3c6816972be27109f6926f601101e11db8
Jiwoong Kim <jkim23@bunny.swmed.edu>
2019-02-16 10:58:54 -0600

    first commit

--- example.sh
+++ example.sh
@@ -7,7 +7,7 @@ perl VAMP_database.pl
 # 3. Identify AMR genotypes
 perl VAMP.pl Klebsiella_pneumoniae_MGH_43.fasta > Klebsiella_pneumoniae_MGH_43.VAMP.txt
 
-# 4. Download VAMPr prediction model - you can find the download links in http://share.zhanxw.com/VAMPr/VAMPr.cgi?show=model
+# 4. Download VAMPr prediction model
 wget http://share.zhanxw.com/VAMPr/VAMP_model/573.amikacin.VAMP_model.RData
 wget http://share.zhanxw.com/VAMPr/VAMP_model/573.cefepime.VAMP_model.RData
 
